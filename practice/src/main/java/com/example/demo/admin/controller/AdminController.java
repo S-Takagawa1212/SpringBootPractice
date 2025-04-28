cacec0c6ba7c9b842649a563e79ac27ce336dad6
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-28 23:42:58 +0900

    feat(controller): contacts/{id}へのコントローラーの定型を作成した

--- practice/src/main/java/com/example/demo/admin/controller/AdminController.java
+++ practice/src/main/java/com/example/demo/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.example.demo.admin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.admin.service.AdminService;
 
@@ -20,6 +21,14 @@ public class AdminController {
         model.addAttribute("contactDataForAdmin", adminService.getAllContact());
 
         return "admin/contactList";
+    }
+
+    @GetMapping("admin/contacts/{id}")
+    public String showContactsDeteil(@PathVariable("id") Long id, Model model) {
+
+        // TODO idをkeyにしてDBから情報を取得して、それをmodelに格納する
+
+        return "admin/contactDetail";
     }
 
 }
