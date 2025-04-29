0eb8487b2ccc9f481acfb1f49bddf423345b66cd
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-29 16:34:43 +0900

    feat(controller): editContactの雛形を実装

--- practice/src/main/java/com/example/demo/admin/controller/AdminController.java
+++ practice/src/main/java/com/example/demo/admin/controller/AdminController.java
@@ -3,9 +3,12 @@ package com.example.demo.admin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.admin.service.AdminService;
+import com.example.demo.contact.form.ContactForm;
 
 import lombok.RequiredArgsConstructor;
 
@@ -39,6 +42,14 @@ public class AdminController {
         model.addAttribute("contactDataDetailForAdmin", adminService.getContactById(id));
 
         return "admin/contactEdit";
+    }
+
+    @PostMapping("admin/contacts/{id}/edit")
+    public String editContact(@ModelAttribute ContactForm form, @PathVariable Long id) {
+        //TODO:DBのデータを上書きする
+        System.out.println("データを上書きしたってばよ！");
+        // TODO 上書きしました、という表示と変更後のデータが入力されたhtmlを表示させる
+        return "redirect:/admin/contacts";
     }
 
 }
